92177d359e908aed952067a428da02d70bdf0742
Martin Patino <mpatino117@gmail.com>
2016-07-24 19:36:18 -0700

    fixed http

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] === 'https') {
+    if (req.headers['x-forwarded-proto'] === 'http') {
 
         next();
 
     } else {
 
-        res.redirect('https://', req.hostname + req.url);
+        res.redirect('http://', req.hostname + req.url);
     }
 });
 
