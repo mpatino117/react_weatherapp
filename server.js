303ee2e1790d58fcea4247ccfe62865c6bc8b71a
Martin Patino <mpatino117@gmail.com>
2016-07-24 19:23:50 -0700

    make it better

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
+    if (req.headers['x-forwarded-proto'] === 'https') {
 
         next();
+
     } else {
 
-        res.redirect('https://', req.hostname, + '/' + req.url);
+        res.redirect('https://', req.hostname + req.url);
     }
 });
 
