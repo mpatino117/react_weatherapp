79085c2ff48f16b12609d788683505a9b95034ce
Martin Patino <mpatino117@gmail.com>
2016-07-24 17:22:33 -0700

    http horoku

--- server.js
+++ server.js
@@ -3,7 +3,18 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+
+        next();
+    } else {
+
+        res.redirect('https://', req.hostname, req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Express server at Port 3000 is running'));
+app.listen(PORT, () => console.log('Express Server At Port' + PORT + 'Is Running \n \n  Local: http://localhost:' + PORT));
